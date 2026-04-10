950e55228ee9746e0eda3336ac94f4d97c5e257d
Harry Koehler <harry.koehler@gmail.com>
2026-04-10 12:53:48 +0200

    3

--- src/main/scala/com/example/CubeCalculator.scala
+++ src/main/scala/com/example/CubeCalculator.scala
@@ -2,3 +2,4 @@ object CubeCalculator {
   def cube(x: Int): Int =
     x * x * x
 }
+// This is a simple Scala object that defines a method `cube` which takes an integer `x` and returns its cube by multiplying `x` by itself three times.
